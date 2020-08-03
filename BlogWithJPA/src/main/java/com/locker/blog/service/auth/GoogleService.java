c4be014df8fe2be8a304b4754de86e764e318184
junhok82 <junhok82@gmail.com>
2020-08-03 17:54:35 +0900

    Feat : Update social login service

--- BlogWithJPA/src/main/java/com/locker/blog/service/auth/GoogleService.java
+++ BlogWithJPA/src/main/java/com/locker/blog/service/auth/GoogleService.java
@@ -1,2 +1,84 @@
-package com.locker.blog.service.auth;public class GoogleService {
+package com.locker.blog.service.auth;
+
+import com.google.gson.Gson;
+import com.locker.blog.advice.exception.CCommunicationException;
+import com.locker.blog.domain.social.RetAuth;
+import com.locker.blog.domain.user.KakaoProfile;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Service
+public class GoogleService {
+
+    private final RestTemplate restTemplate;
+    private final Environment env;
+    private final Gson gson;
+
+    @Value("${spring.url.base}")
+    private String baseUrl;
+
+    @Value("${spring.security.oauth2.client.registration.google.clientId}")
+    private String googleClientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.clientSecret}")
+    private String googleClientSecret;
+
+    @Value("${spring.security.oauth2.client.registration.google.redirectUri}")
+    private String googleRedirect;
+
+    public KakaoProfile getGoogleProfile(String accessToken) {
+        // Set header : Content-type: application/x-www-form-urlencoded
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.set("Authorization", "Bearer " + accessToken);
+
+        // Set http entity
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(null, headers);
+        try {
+            // Request profile
+            ResponseEntity<String> response = restTemplate.postForEntity(env.getProperty("spring.social.kakao.url.profile"), request, String.class);
+            if (response.getStatusCode() == HttpStatus.OK)
+                return gson.fromJson(response.getBody(), KakaoProfile.class);
+        } catch (Exception e) {
+            throw new CCommunicationException();
+        }
+        throw new CCommunicationException();
+    }
+
+    public RetAuth getGoogleTokenInfo(String code) {
+        // Set header : Content-type: application/x-www-form-urlencoded
+        System.out.println(code);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        // Set parameter
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("code", code);
+        params.add("client_id", googleClientId);
+        params.add("client_secret", googleClientSecret);
+        params.add("redirect_uri", googleRedirect);
+        params.add("grant_type", "authorization_code");
+
+        // Set http entity
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+        URI uri = URI.create("https://www.googleapis.com/oauth2/v4/token");
+
+        ResponseEntity<String> response = restTemplate.postForEntity(uri, request, String.class);
+
+        System.out.println(response.getBody());
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return gson.fromJson(response.getBody(), RetAuth.class);
+        }
+        return null;
+    }
 }
--- BlogWithJPA/src/main/java/com/locker/blog/service/auth/KakaoService.java
+++ BlogWithJPA/src/main/java/com/locker/blog/service/auth/KakaoService.java
@@ -1,4 +1,4 @@
-package com.locker.blog.service.user;
+package com.locker.blog.service.auth;
 
 import com.google.gson.Gson;
 import com.locker.blog.advice.exception.CCommunicationException;
