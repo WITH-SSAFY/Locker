3bd8bfb8816d93bcbc732e0a2fa36455003c6c1b
junhok82 <junhok82@gmail.com>
2020-08-03 22:58:04 +0900

    Feat : Create DTO for github's user infomation

--- BlogWithJPA/src/main/java/com/locker/blog/domain/social/GithubRetAuth.java
+++ BlogWithJPA/src/main/java/com/locker/blog/domain/social/GithubRetAuth.java
@@ -1,2 +1,11 @@
-package com.locker.blog.domain.social;public class GithubRetAuth {
+package com.locker.blog.domain.social;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class GithubRetAuth {
+    private String access_token;
+    private String token_type;
+    private String scope;
 }
