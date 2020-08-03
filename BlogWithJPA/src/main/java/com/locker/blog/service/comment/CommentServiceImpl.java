b7d9d995cf6bb582656e32a94bf273b09710e423
배현석 <qogustjr119@naver.com>
2020-08-03 17:49:38 +0900

    Feat: 어노테이션 추가
    내뇽:
    - Repo에@Repository Service에 @Service 추가

--- BlogWithJPA/src/main/java/com/locker/blog/service/comment/CommentServiceImpl.java
+++ BlogWithJPA/src/main/java/com/locker/blog/service/comment/CommentServiceImpl.java
@@ -3,9 +3,10 @@ package com.locker.blog.service.comment;
 import com.locker.blog.domain.comment.Comment;
 import com.locker.blog.repository.comment.CommentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class CommentServiceImpl implements CommentService{
     @Autowired
     CommentRepo commentRepo;
