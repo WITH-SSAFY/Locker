9dd069c0ac0eb74231d4dd2a52b267e5cf32023c
junhok82 <junhok82@gmail.com>
2020-08-03 17:53:43 +0900

    Feat : Update user domain for Oauth2

--- BlogWithJPA/src/main/java/com/locker/blog/domain/auth/SessionUser.java
+++ BlogWithJPA/src/main/java/com/locker/blog/domain/auth/SessionUser.java
@@ -1,2 +1,19 @@
-package com.locker.blog.domain.auth;public class SessionUser {
+package com.locker.blog.domain.auth;
+
+import com.locker.blog.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
 }
--- BlogWithJPA/src/main/java/com/locker/blog/domain/social/RetAuth.java
+++ BlogWithJPA/src/main/java/com/locker/blog/domain/social/RetAuth.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
-public class RetKakaoAuth {
+public class RetAuth {
     private String access_token;
     private String token_type;
     private String refresh_token;
