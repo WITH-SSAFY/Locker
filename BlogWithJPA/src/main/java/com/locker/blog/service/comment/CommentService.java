7d1012b6cb39102a1c3cca5653a798bcf31a48d0
배현석 <qogustjr119@naver.com>
2020-08-03 16:52:49 +0900

    Feat: 댓글 repo 및 service 생성

---
+++ BlogWithJPA/src/main/java/com/locker/blog/service/comment/CommentService.java
@@ -0,0 +1,12 @@
+package com.locker.blog.service.comment;
+
+import com.locker.blog.domain.comment.Comment;
+
+import java.util.List;
+
+public interface CommentService {
+    public int insert(Comment comment);//댓글 추가
+    public int delete(Comment comment);//삭제
+    public List<Comment> selectAll();//모든 댓글 불러오기
+    public int update(Comment comment);//댓글 수정
+}
---
+++ BlogWithJPA/src/main/java/com/locker/blog/service/comment/CommentServiceImpl.java
@@ -0,0 +1,32 @@
+package com.locker.blog.service.comment;
+
+import com.locker.blog.domain.comment.Comment;
+import com.locker.blog.repository.comment.CommentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CommentServiceImpl implements CommentService{
+    @Autowired
+    CommentRepo commentRepo;
+
+    @Override
+    public int insert(Comment comment) {
+        return commentRepo.insert(comment);
+    }
+
+    @Override
+    public int delete(Comment comment) {
+        return commentRepo.delete(comment);
+    }
+
+    @Override
+    public List<Comment> selectAll() {
+        return commentRepo.selectAll();
+    }
+
+    @Override
+    public int update(Comment comment) {
+        return commentRepo.update(comment);
+    }
+}
