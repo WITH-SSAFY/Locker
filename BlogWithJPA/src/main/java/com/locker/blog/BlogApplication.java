1b7f9eac8175cb0dc4151b220e5308df2a59215d
junhok82 <junhok82@gmail.com>
2020-08-05 03:14:16 +0900

    Fix : config files

--- BlogWithJPA/src/main/java/com/locker/blog/BlogApplication.java
+++ BlogWithJPA/src/main/java/com/locker/blog/BlogApplication.java
@@ -3,6 +3,7 @@ package com.locker.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
