a5ff8eb452c2534ff51c2fd8d172580e77dbc20b
ynnjn <myung2ing@gmail.com>
2020-08-05 15:19:52 +0900

    Feat : 메인 포스트리스트의 그리드 수정 - Bootstrap의 justify-content => Bootstrap의 offset - 반응형 디자인 개선 필요

--- frontend/src/views/contents/mainPostList.vue
+++ frontend/src/views/contents/mainPostList.vue
@@ -2,22 +2,54 @@
   <v-container>
     <div class="row">
       <div class="col-md-9">
-        <v-flex style="margin-top: 0;">
+        <v-flex style="margin: 0;">
           <h1 class="mb-5" style="font-size: 3.3rem;"><strong>LOCKER</strong></h1>
           <!-- <h1 class="ml-2 mb-5" style="posigion: absolute; font-size: 4rem;"><strong>LOCKER</strong></h1> -->
           <div class="main-under-line"></div>
         </v-flex>
 
-        <div class="row justify-content-end">
-          <div class="col-md-3 m-2" style="height: 25vh; background-color: #333;"></div>
-          <div class="col-md-3 m-2" style="height: 25vh; background-color: #333;"></div>
-          <div class="col-md-3 m-2" style="height: 25vh; background-color: #333;"></div>
+       
+        <div class="row no-gutters">
+          <div class="col-md-3 offfset-md-3" style="height: 25vh;">
+
+          </div>
+          <div class="box col-md-3" style="background-color: #333;">
+            <div class="thumbnail">
+              <img src="#">
+            </div>
+          </div>
+          <div class="box col-md-3" style="background-color: #333;">
+            <div class="thumbnail">
+              <img src="#">
+            </div>
+          </div>
+          <div class="box col-md-3" style="background-color: #333;">
+            <div class="thumbnail">
+              <img src="#">
+            </div>
+          </div>
         </div>
-        <div class="row justify-content-between">
-          <div class="col-md-3 m-2" style="height: 25vh; background-color: #333;"></div>
-          <div class="col-md-3 ml-md-auto m-2" style="height: 25vh; background-color: #333;"></div>
-          <div class="col-md-3 m-2" style="height: 25vh; background-color: #333;"></div>
+        <div class="row no-gutters">
+          <div class="box col-md-3" style="background-color: #333;">
+            <div class="thumbnail">
+              <img src="#">
+            </div>
+          </div>
+          <div class="col-md-3 offfset-md-3" style="height: 25vh;">
+
+          </div>
+          <div class="box col-md-3" style="background-color: #333;">
+            <div class="thumbnail">
+              <img src="#">
+            </div>
+          </div>
+          <div class="box col-md-3" style="background-color: #333;">
+            <div class="thumbnail">
+              <img src="#">
+            </div>
+          </div>
         </div>
+        
         <!-- <v-item-group
           v-model="selected"
           :mandatory="mandatory"
@@ -185,5 +217,9 @@ export default {
 </script>
 
 <style>
-
+.box {
+  box-sizing: border-box;
+  border: 5px solid transparent;
+  background-clip: padding-box;
+}
 </style>
