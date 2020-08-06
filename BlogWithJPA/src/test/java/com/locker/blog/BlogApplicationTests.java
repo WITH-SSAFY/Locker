6d2f616725c1e49890841729318eddc4c72ac7c7
junhok82 <junhok82@gmail.com>
2020-08-06 12:54:59 +0900

    Deploy test with jenkins

--- BlogWithJPA/src/test/java/com/locker/blog/BlogApplicationTests.java
+++ BlogWithJPA/src/test/java/com/locker/blog/BlogApplicationTests.java
@@ -1,9 +1,15 @@
 package com.locker.blog;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlogApplicationTests {
-
+    @Test
+    public void deployTest() {
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+        logger.info("deploy - success");
+    }
 }
