e1f50a98e1cd0c5155f5ed5967609330be67ba39
배현석 <qogustjr119@naver.com>
2020-08-08 14:58:08 +0900

    Test: https 테스트

--- frontend/src/lib/axios-common.js
+++ frontend/src/lib/axios-common.js
@@ -1,9 +1,10 @@
-import axios from 'axios'
+import axios from "axios";
 
 export default axios.create({
-    // baseURL: "http://i3a606.p.ssafy.io:8000/api",
-    baseURL: "https://i3a606.p.ssafy.io/api",
-    headers:{
-        "Content-type": "application/json",
-    }
+  // baseURL: "http://i3a606.p.ssafy.io:8000/api",
+  // baseURL: "https://i3a606.p.ssafy.io/api",
+  baseURL: "http://i3a606.p.ssafy.io:8000/api",
+  headers: {
+    "Content-type": "application/json",
+  },
 });
