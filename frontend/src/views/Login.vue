f08880e85397ec190685b79e86e080d237459994
윤재원 <jane3999591@gmail.com>
2020-08-10 14:30:08 +0900

    Feature : naver login sdk

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -180,6 +180,16 @@ export default {
       that.isSignIn = that.$gAuth.isAuthorized;
       if (that.isInit) clearInterval(checkGauthLoad);
     }, 1000);
+
+    var naverLogin = new naver.LoginWithNaverId({
+        clientId: "{YOUR_CLIENT_ID}",
+        callbackUrl: "{YOUR_REDIRECT_URL}",
+        isPopup: true, /* 팝업을 통한 연동처리 여부 */
+        loginButton: {color: "green", type: 3, height: 60} /* 로그인 버튼의 타입을 지정 */
+    });
+    
+    /* 설정정보를 초기화하고 연동을 준비 */
+    naverLogin.init();
   }
 }
 </script>
