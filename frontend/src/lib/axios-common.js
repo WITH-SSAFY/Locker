15d3f71c8f79476ed30eb84e47bef9d14d7e3bae
배현석 <qogustjr119@naver.com>
2020-08-11 17:07:30 +0900

    axios baseURL 원상 복구

--- frontend/src/lib/axios-common.js
+++ frontend/src/lib/axios-common.js
@@ -3,8 +3,7 @@ import axios from "axios";
 export default axios.create({
   // baseURL: "http://i3a606.p.ssafy.io:8000/api",
   // baseURL: "https://i3a606.p.ssafy.io/api",
-  //baseURL: "http://i3a606.p.ssafy.io:8000/api",
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://i3a606.p.ssafy.io:8000/api",
 
   headers: {
     "Content-type": "application/json",
