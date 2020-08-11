d54283f6699ba4b6d442e40964968df3c2b34c12
Jaewon <jane399@naver.com>
2020-08-11 14:08:49 +0900

    Test: Test for github login

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,38 +3,41 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import BootstrapVue from 'bootstrap-vue';
-import VueFullPage from 'vue-fullpage.js';
-import GAuth from 'vue-google-oauth2';
+import BootstrapVue from "bootstrap-vue";
+import VueFullPage from "vue-fullpage.js";
+import GAuth from "vue-google-oauth2";
+// import Github from "./router/github.js";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import 'codemirror/lib/codemirror.css';
-import '@toast-ui/editor/dist/toastui-editor.css';
-import '@toast-ui/editor/dist/toastui-editor-viewer.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "codemirror/lib/codemirror.css";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import "@toast-ui/editor/dist/toastui-editor-viewer.css";
 // import 'swiper/swiper-bundle.css';
 
-const gquthOption={
-  clientId: '858388202728-f26u33io27sn79ic2ac789sc03ht9vuf.apps.googleusercontent.com',
+const gquthOption = {
+  clientId:
+    "858388202728-f26u33io27sn79ic2ac789sc03ht9vuf.apps.googleusercontent.com",
   // clientId: '768834812579-007e5802er7gj3c93p8qa9568h8bj3na.apps.googleusercontent.com',
-  scope: 'profile email', 
-  prompt: 'select_account',
-  fetch_basic_profile: false
-}
+  scope: "profile email",
+  prompt: "select_account",
+  fetch_basic_profile: false,
+};
 
 Vue.use(GAuth, gquthOption);
 Vue.use(VueFullPage);
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
+// Vue.use(Github);
 
 new Vue({
   router,
   store,
-  beforeCreate(){
-    this.$store.dispatch('getMemberInfo')
+  beforeCreate() {
+    this.$store.dispatch("getMemberInfo");
   },
   vuetify,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
 
 // new Swiper('.swiper-container');
