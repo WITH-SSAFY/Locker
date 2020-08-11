b48ef84f61397eb03604e60d9d41682e37765156
Jaewon <jane3999591@gmail.com>
2020-08-11 18:41:42 +0900

    Test: git-login

---
+++ frontend/src/views/Github.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>hello</div>
+</template>
+
+<script>
+import axios from "../lib/axios-common.js";
+
+export default {
+  beforeRouteEnter(to, from, next) {
+    console.log("to from next", { to, from, next });
+    axios
+      .get("/api/social/github/token")
+      .then(function(res) {
+        console.log("res", res.data);
+        //res.data.token
+      })
+      .catch(function(err) {
+        console.log("err", err);
+      });
+  }
+};
+</script>
