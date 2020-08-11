d90ec62add7ea598139b46c2eb20bf8c3afa3f46
Jaewon <jane3999591@gmail.com>
2020-08-11 23:45:57 +0900

    Fix: github login test

--- frontend/src/views/Github.vue
+++ frontend/src/views/Github.vue
@@ -10,7 +10,7 @@ export default {
   methods: {
     ...mapActions(["signinWithSocial"])
   },
-  beforeRouteEnter() {
+  beforeResolve() {
     axios
       .get("/social/github/token")
       .then(function(res) {
