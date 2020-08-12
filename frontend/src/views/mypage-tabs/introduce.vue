2c37428dae149f30f0af1dc06b39a3961e53f02b
YNNJN <dbswls1014@naver.com>
2020-08-12 18:28:12 +0900

    Test : 마크다운 뷰어 테스트

--- frontend/src/views/mypage-tabs/introduce.vue
+++ frontend/src/views/mypage-tabs/introduce.vue
@@ -1,24 +1,35 @@
 <template>
   <div>
     introduce test
-    {{ intro }}
+    <!-- {{ intro }} -->
+    <div style="height: 1000px;">
+      <viewer :initialValue="intro" height="100%" />
+    </div>
+
   </div>
 </template>
 
 <script>
 import axios from "../../lib/axios-common.js";
+import { Viewer } from '@toast-ui/vue-editor';
+import '@toast-ui/editor/dist/toastui-editor-viewer.css';
 
 export default {
+  name: 'introcduce',
   data() {
     return {
       intro: '',
     }
   },
   created () {
-    this.getIntro()
+    this.getIntro();
+  
   },
   computed: {
-    
+
+  },
+  components: {
+    Viewer,
   },
   methods: {
     // access token을 로컬스토리지에서 가져와서 넘겨주기
@@ -27,10 +38,9 @@ export default {
       // test
       let accessToken = '055bcf6a87cfd6b760b67a2150cfb5948b5b9385';
       console.log(accessToken)
-      axios
-        .get("/v1/github/hidden?accessToken=" + accessToken)
+      axios.get("/v1/github/hidden?accessToken=" + accessToken)
         .then((response) => {
-          this.intro = response.data;
+          this.intro = response.data.data
         })
         .catch((exp) => alert("소개글 불러오기 실패" + exp))
     }
