4eeb52010bd1824ed3db22633915178ed56d8ccf
YNNJN <dbswls1014@naver.com>
2020-08-13 11:24:49 +0900

    Feat : 메인페이지 락커 높이 조정

--- frontend/src/views/contents/mainPostList.vue
+++ frontend/src/views/contents/mainPostList.vue
@@ -1,124 +1,131 @@
 <template>
   <div class="">
-    <div style="">
-      <div class="cont s--inactive">
-        <!-- cont inner start -->
-        <div class="cont__inner">
-          <!-- el start -->
-          <div class="el">
-            <div class="el__overflow">
-              <div class="el__inner">
-                <div class="el__bg"></div>
-                <div class="el__preview-cont">
-                  <h2 class="el__heading">Section 1</h2>
-                </div>
-                <div class="el__content">
-                  <div class="el__text">Whatever</div>
-                  <div class="el__close-btn"></div>
-                </div>
+    <!-- <div class="">
+      <h1 style="margin: 0 3rem; font-size: 3.3rem;"><strong>LOCKER</strong></h1>
+      <div class="main-under-line my-3" style="margin: 0 3rem;"></div>
+    </div> -->
+    <!-- <div class="m-5">
+      <p style="positon: relative; font-size: 1.5rem;">The 5 Best Repositories</p>
+    </div> -->
+    <div class="cont s--inactive">
+      <!-- cont inner start -->
+      <div class="cont__inner">
+        <!-- el start -->
+        <div class="el">
+          <div class="el__overflow">
+            <div class="el__inner">
+              <div class="el__bg"></div>
+              <div class="el__preview-cont">
+                <h2 class="el__heading">Section 1</h2>
               </div>
-            </div>
-            <div class="el__index">
-              <div class="el__index-back">1</div>
-              <div class="el__index-front">
-                <div class="el__index-overlay" data-index="1">1</div>
-              </div>
-            </div>
-          </div>
-          <!-- el end -->
-          <!-- el start -->
-          <div class="el">
-            <div class="el__overflow">
-              <div class="el__inner">
-                <div class="el__bg"></div>
-                <div class="el__preview-cont">
-                  <h2 class="el__heading">Section 2</h2>
-                </div>
-                <div class="el__content">
-                  <div class="el__text">Whatever</div>
-                  <div class="el__close-btn"></div>
-                </div>
-              </div>
-            </div>
-            <div class="el__index">
-              <div class="el__index-back">2</div>
-              <div class="el__index-front">
-                <div class="el__index-overlay" data-index="2">2</div>
+              <div class="el__content">
+                <div class="el__text">Whatever</div>
+                <div class="el__close-btn"></div>
               </div>
             </div>
           </div>
-          <!-- el end -->
-          <!-- el start -->
-          <div class="el">
-            <div class="el__overflow">
-              <div class="el__inner">
-                <div class="el__bg"></div>
-                <div class="el__preview-cont">
-                  <h2 class="el__heading">Section 3</h2>
-                </div>
-                <div class="el__content">
-                  <div class="el__text">Whatever</div>
-                  <div class="el__close-btn"></div>
-                </div>
-              </div>
-            </div>
-            <div class="el__index">
-              <div class="el__index-back">3</div>
-              <div class="el__index-front">
-                <div class="el__index-overlay" data-index="3">3</div>
-              </div>
+          <div class="el__index">
+            <div class="el__index-back">1</div>
+            <div class="el__index-front">
+              <div class="el__index-overlay" data-index="1">1</div>
             </div>
           </div>
-          <!-- el end -->
-          <!-- el start -->
-          <div class="el">
-            <div class="el__overflow">
-              <div class="el__inner">
-                <div class="el__bg"></div>
-                <div class="el__preview-cont">
-                  <h2 class="el__heading">Section 4</h2>
-                </div>
-                <div class="el__content">
-                  <div class="el__text">Whatever</div>
-                  <div class="el__close-btn"></div>
-                </div>
-              </div>
-            </div>
-            <div class="el__index">
-              <div class="el__index-back">4</div>
-              <div class="el__index-front">
-                <div class="el__index-overlay" data-index="4">4</div>
-              </div>
-            </div>
-          </div>
-          <!-- el end -->
-          <!-- el start -->
-          <div class="el">
-            <div class="el__overflow">
-              <div class="el__inner">
-                <div class="el__bg"></div>
-                <div class="el__preview-cont">
-                  <h2 class="el__heading">Section 5</h2>
-                </div>
-                <div class="el__content">
-                  <div class="el__text">Whatever</div>
-                  <div class="el__close-btn"></div>
-                </div>
-              </div>
-            </div>
-            <div class="el__index">
-              <div class="el__index-back">5</div>
-              <div class="el__index-front">
-                <div class="el__index-overlay" data-index="5">5</div>
-              </div>
-            </div>
-          </div>
-          <!-- el end -->
         </div>
-        <!-- cont inner end -->
+        <!-- el end -->
+        <!-- el start -->
+        <div class="el">
+          <div class="el__overflow">
+            <div class="el__inner">
+              <div class="el__bg"></div>
+              <div class="el__preview-cont">
+                <h2 class="el__heading">Section 2</h2>
+              </div>
+              <div class="el__content">
+                <div class="el__text">Whatever</div>
+                <div class="el__close-btn"></div>
+              </div>
+            </div>
+          </div>
+          <div class="el__index">
+            <div class="el__index-back">2</div>
+            <div class="el__index-front">
+              <div class="el__index-overlay" data-index="2">2</div>
+            </div>
+          </div>
+        </div>
+        <!-- el end -->
+        <!-- el start -->
+        <div class="el">
+          <div class="el__overflow">
+            <div class="el__inner">
+              <div class="el__bg"></div>
+              <div class="el__preview-cont">
+                <h2 class="el__heading">Section 3</h2>
+              </div>
+              <div class="el__content">
+                <div class="el__text">Whatever</div>
+                <div class="el__close-btn"></div>
+              </div>
+            </div>
+          </div>
+          <div class="el__index">
+            <div class="el__index-back">3</div>
+            <div class="el__index-front">
+              <div class="el__index-overlay" data-index="3">3</div>
+            </div>
+          </div>
+        </div>
+        <!-- el end -->
+        <!-- el start -->
+        <div class="el">
+          <div class="el__overflow">
+            <div class="el__inner">
+              <div class="el__bg"></div>
+              <div class="el__preview-cont">
+                <h2 class="el__heading">Section 4</h2>
+              </div>
+              <div class="el__content">
+                <div class="el__text">Whatever</div>
+                <div class="el__close-btn"></div>
+              </div>
+            </div>
+          </div>
+          <div class="el__index">
+            <div class="el__index-back">4</div>
+            <div class="el__index-front">
+              <div class="el__index-overlay" data-index="4">4</div>
+            </div>
+          </div>
+        </div>
+        <!-- el end -->
+        <!-- el start -->
+        <div class="el">
+          <div class="el__overflow">
+            <div class="el__inner">
+              <div class="el__bg"></div>
+              <div class="el__preview-cont">
+                <h2 class="el__heading">Section 5</h2>
+              </div>
+              <div class="el__content">
+                <div class="el__text">Whatever</div>
+                <div class="el__close-btn"></div>
+              </div>
+            </div>
+          </div>
+          <div class="el__index">
+            <div class="el__index-back">5</div>
+            <div class="el__index-front">
+              <div class="el__index-overlay" data-index="5">5</div>
+            </div>
+          </div>
+        </div>
+        <!-- el end -->
       </div>
+      <!-- cont inner end -->
+      
     </div>
   </div>
+  
 
 </template>
 
