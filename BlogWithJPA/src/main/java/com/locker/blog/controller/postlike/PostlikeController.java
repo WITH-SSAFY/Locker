3d2e86c2e0fc503a152d436b24e15849f9523aa6
배현석 <qogustjr119@naver.com>
2020-08-15 13:04:04 +0900

    fix: 스웨거 번호 오류 수정

--- BlogWithJPA/src/main/java/com/locker/blog/controller/postlike/PostlikeController.java
+++ BlogWithJPA/src/main/java/com/locker/blog/controller/postlike/PostlikeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@Api(tags = {"6. Postlike"})
+@Api(tags = {"7. Postlike"})
 @RestController
 @RequestMapping("/api/v1/postlike")
 @CrossOrigin
