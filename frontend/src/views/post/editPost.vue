c4afb0b980f988ebcfdd1f5db6b381cd1133280d
배현석 <qogustjr119@naver.com>
2020-08-16 18:24:21 +0900

    Feat: 글 수정 창 기능 추가
    내용:
    - 기존 포스트에 등록된 태그들 로딩
    - 글 수정후 afterPost로 이동 isNewPost = false(새로 작성된 글이 아님을
      표시)

--- frontend/src/views/post/editPost.vue
+++ frontend/src/views/post/editPost.vue
@@ -1,40 +1,43 @@
 <template>
   <div id="container">
     <div id="title_container">
-      <v-text-field
-      id="title"
-      v-model="title"
-      height="50"
-      >
-      </v-text-field>
+      <v-text-field id="title" v-model="title"></v-text-field>
     </div>
-    
+    <div id="tag_list">
+      <v-chip
+        v-for="(tag, index) in tags"
+        :key="index"
+        class="mx-1"
+        color="#EDE7F6"
+        small
+        @click="deleteTag(index)"
+      >{{ tag }}</v-chip>
+    </div>
+
+    <div id="tag_container">
+      <v-text-field
+        id="tag"
+        v-model="tag"
+        placeholder="태그를 입력해주세요"
+        height="16"
+        @keyup.enter="createTag"
+      ></v-text-field>
+    </div>
+
     <editor
-        ref="toastuiEditor"
-        id="editor"
-        :initialValue="initContent"
-        :options="editorOptions"
-        height="90%"
-        previewStyle="vertical"
-        @change="onEditorChange"
+      ref="toastuiEditor"
+      id="editor"
+      :initialValue="initContent"
+      :options="editorOptions"
+      height="90%"
+      previewStyle="vertical"
+      @change="onEditorChange"
     />
     <div id="buttons">
-      <v-btn
-        id="temp_save"
-        class="save_button"
-        color="#c7c9c5">
-        임시저장
-      </v-btn>
-      <v-btn
-        id="save"
-        class="save_button"
-        color="#7C4DFF"
-        @click="postContent">
-        수정완료
-      </v-btn>
+      <v-btn id="temp_save" class="save_button" color="#c7c9c5">임시저장</v-btn>
+      <v-btn id="save" class="save_button" color="#7C4DFF" @click="postContent">수정완료</v-btn>
     </div>
   </div>
-    
 </template>
 <script>
 // const defaultOptions = {
@@ -67,102 +70,174 @@
 //     'codeblock'
 //   ]
 //   };
-  import 'codemirror/lib/codemirror.css';
-  import '@toast-ui/editor/dist/toastui-editor.css';
-  //import 'highlight.js/styles/github.css';
+import "codemirror/lib/codemirror.css";
+import "@toast-ui/editor/dist/toastui-editor.css";
+//import 'highlight.js/styles/github.css';
 
-  import { Editor } from '@toast-ui/vue-editor';
-  import axios from "../../lib/axios-common.js"; //axios : 서버와 통신하기 위함
-  
-  export default {
-    created(){
-        this.title;
-        this.initContent;
-        this.pid;
-    },
-    components: {
-      editor: Editor,
-    },
-    data() {
-      return {
-        content: "",
-        editorOptions: {
-          hideModeSwitch: true//모드 설정(markdown, wysiwyg) 안보이게함
-        }
-      };
-    },
-    computed: {
-        title(){
-            return this.$store.state.myDetailTitle;
-        },
+import { Editor } from "@toast-ui/vue-editor";
+import axios from "../../lib/axios-common.js"; //axios : 서버와 통신하기 위함
 
-        initContent(){
-            return this.$store.state.myDetail;
-        },
-        pid(){
-            return this.$store.state.pid;
-        }
-
-    },
-    methods: {
-      onEditorChange() {//에디터의 content가 바뀌었을 때
-        //자동 저장 구현(나중에)
-      },
-      postContent(){
-        this.content = this.$refs.toastuiEditor.invoke('getMarkdown');
-        console.log("editPost_pid: "+this.pid)
-        axios
-          .put("/v1/post",{title : this.title,
-                            content: this.content,
-                            pid: this.pid})
-           .then(() =>{
-             //alert("작성자: "+this.$store.state.userInfo.nickname);
-             //this.$store.dispatch('showMyDetail', this.pid);
-             //this.$store.dispatch("getMyPostList");
-             this.$router.push({name:"mypage"}); //어디로 이동해야할지 정해야할듯?  
-           })
-          .catch(exp => alert("글 작성에 실패했습니다"+exp));
+export default {
+  created() {
+    this.title = this.$store.state.myDetailTitle;
+    this.initContent;
+    this.pid;
+    this.thumbnamil;
+    this.description;
+    this.getTags();
+  },
+  components: {
+    editor: Editor
+  },
+  data() {
+    return {
+      title: "",
+      content: "",
+      tags: [],
+      tag: "",
+      editorOptions: {
+        hideModeSwitch: true //모드 설정(markdown, wysiwyg) 안보이게함
       }
+    };
+  },
+  computed: {
+    initContent() {
+      return this.$store.state.myDetail;
+    },
+    pid() {
+      return this.$store.state.pid;
+    },
+    thumbnamil() {
+      return this.$store.state.thumbnail;
+    },
+    description() {
+      return this.$store.state.description;
+    }
+  },
+  methods: {
+    onEditorChange() {
+      //에디터의 content가 바뀌었을 때
+      //자동 저장 구현(나중에)
+    },
+    postContent() {
+      //포스트에 등록된 썸네일
+
+      this.content = this.$refs.toastuiEditor.invoke("getMarkdown");
+      this.$store.state.myPost = {
+        pid: this.pid,
+        title: this.title,
+        email: this.$store.state.userInfo.email,
+        content: this.content,
+        nickname: this.$store.state.userInfo.nickname,
+        thumbnail: this.$store.state.thumbnail,
+        description: this.$store.state.description
+      };
+
+      this.$store.state.myTags = this.tags;
+      this.$store.state.isNewPost = false;
+
+      this.$router.push({ name: "afterPost" });
+    },
+    getTags() {
+      //기존에 해당 포스트에 등록된 태그 받아옴
+      axios.get("/v1/tag/all/" + this.pid).then(response => {
+        this.tags = response.data;
+      });
+    },
+    createTag() {
+      //태그 생성
+      //중복 확인
+      let isDup = false; //중복 되었는가?
+
+      for (let i = 0; i < this.tags.length; i++) {
+        if (this.tags[i] == this.tag) {
+          isDup = true;
+          break;
+        }
+      }
+
+      if (!isDup && this.tag != "") {
+        //중복과 빈 값 인지 체크
+        this.tags.push(this.tag);
+        this.tag = "";
+      }
+    },
+    deleteTag(index) {
+      //태그 삭제
+      this.tags.splice(index, 1); //splice(p1,p2,p3) p1: 시작 index, p2: 삭제 or 수정할 요소 개수, p3 바꿀 내용
+    }
   }
-}
+};
 </script>
 
 <style scoped>
-    #container {
-       margin: 20px auto 0 auto;
-       height: 100%;
-       width: 100%;
-       padding: 0 55px 0 62px;
-    } 
+#container {
+  margin: 20px auto 0 auto;
+  height: 100%;
+  width: 100%;
+  padding: 0 55px 0 62px;
+}
 
-    #title_container{
-      width: 50%;
-    }
-    .v-input {
-      font-size: 28px;
-      font-weight: 800;
-    }
+#title_container {
+  width: 50%;
+}
+#title {
+  font-size: 27px;
+  font-weight: 800;
+}
+#tag_container {
+  width: 50%;
+}
+#tag_list {
+  width: 50%;
+}
+.mx-1 {
+  margin-top: 3px;
+}
 
-    #editor{
-      margin-bottom: 20px;
-    }
+#tag {
+  font-size: 14px;
+}
 
-    #buttons{
-      text-align: right;
-      margin-right: 5px;
-    }
-    .save_button {
-      margin-left: 7px;
-      margin-bottom: 20px;
-      width : 80pgit chx;
-      color:rgba(255, 255, 255, 0.904);
-    }
+#editor {
+  margin-bottom: 20px;
+}
 
-    @media (max-width: 768px){
-    #title_container{
-      width: 100%;
-    }  
+#buttons {
+  text-align: right;
+  margin-right: 5px;
+}
+.save_button {
+  margin-left: 7px;
+  margin-bottom: 20px;
+  width: 80pgit chx;
+  color: rgba(255, 255, 255, 0.904);
+}
+
+/*md 사이즈 */
+@media (max-width: 768px) {
+  /* 제목 너비 크기 조정 */
+  #title_container {
+    width: 100%;
+  }
+  #tag_container {
+    width: 100%;
+  }
+  #tag_list {
+    width: 100%;
   }
 
-  
+  /*editor 가운데 줄 안보이게 */
+  .tui-editor .te-preview-style-vertical .te-md-splitter {
+    display: none;
+  }
+  /* 오른쪽 결과 창 안보이게 함 */
+  .te-preview {
+    display: none;
+  }
+  /* 왼쪽 에디터 부분 크기 조정*/
+  .tui-editor .te-preview-style-vertical .te-editor {
+    width: 100%;
+  }
+}
 </style>
