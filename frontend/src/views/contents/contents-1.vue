865256943850f1d5d711beefc853dc8cf9d7222a
YNNJN <dbswls1014@naver.com>
2020-08-16 11:43:17 +0900

    Feat : 웹 폰트 적용 & contents1 디자인 수정

--- frontend/src/views/contents/contents-1.vue
+++ frontend/src/views/contents/contents-1.vue
@@ -1,27 +1,28 @@
 <template>
   <div class="contents-1">
     <v-container fill-height style="padding: 0;">
-      <v-layout align-center style="position: relative;">
+      <v-layout style="position: relative; top: 20vh;">
         <div>
           <v-flex class="logo">
-            <v-icon large color="black" class="mb-5">mdi-archive-outline </v-icon>
-            <h1>LOCKER</h1>
+            <v-icon size="50" color="black" class="mb-5">mdi-archive-outline</v-icon>
+            <p class="bold" style="font-size: 3rem;">LOCKER</p>
             <div class="under-line mb-5"></div>
           </v-flex>
-          <v-btn color="#7C4DFF" class="my-5">Getting Started</v-btn>
-          <v-flex>
-            <h5>Already have an accounts?</h5>
-            <a><h5 style="color: #7C4DFF">Sign in</h5></a>
+          <v-btn large color="#7C4DFF" class="my-3"><span class="bold" style="color: #EDE7F6; font-size: 1.5rem;">Getting Started</span></v-btn>
+          <v-flex style="margin-top: 2.2rem;">
+            <p class="medium" style="font-size: 1.1rem;">이미 LOCKER 계정이 있으신가요?</p>
+            <a><p class="bold" style="color: #7C4DFF; font-size: 1.1rem;">로그인하기</p></a>
           </v-flex>
         </div>
 
         <div class="mx-auto ml-5">
           <div class="box-top"></div>
           <div class="box-under"></div>
+          <div class="box-center"></div>
         </div>
-        <div style="position: relative; margin-top: 2.5rem; z-index: 1;">
-          <h1><strong style="font-size: 3.5rem;">Dive deeper on topics</strong></h1>
-          <h1><strong style="font-size: 3.5rem;">that matter to you.</strong></h1>
+        <div style="position: relative; top: 11.5vh; z-index: 1; text-align: right;">
+          <p class="bolder" style="font-size: 3.5rem;">깃헙과 가장 긴밀하게 연결되는</p>
+          <p class="bolder" style="font-size: 3.5rem;">새로운 솔루션, <span style="color: #7C4DFF">LOCKER</span></p>
           <!-- <div class="box-over"></div> -->
         </div>
 
