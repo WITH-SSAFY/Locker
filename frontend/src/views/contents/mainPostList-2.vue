99520bcbac073d0c101d26d61bb81e15e35102d6
YNNJN <dbswls1014@naver.com>
2020-08-18 19:48:48 +0900

    Feat : 메인페이지 전체 글 리스트 디자인 적용 - 그리드 시스템 이용한 카드 형태

--- frontend/src/views/contents/mainPostList-2.vue
+++ frontend/src/views/contents/mainPostList-2.vue
@@ -1,63 +1,64 @@
 <template>
-  <div>
+  <div style="background-color: #eceffc;">
 
-    <ul>
-      <li
-        v-for="post in postList"
-        :key="post.pid"
-      >
-        {{ post.title }}
-        {{ post.content }}
-      </li>    
-    </ul>
+    <div style="margin-left: 3.5rem; margin-top: 3.5rem;">
+      <p class="bold" style="font-size: 1.5rem;"><strong>LOCKER </strong>유저들이 좋아하는 포스트</p>
+      <div class="under-line"></div>
+    </div>
 
+    <div class="w-75" style="margin-left: 3.5rem;">
+      <v-row>
+        <v-col
+          v-for="post in postList"
+          :key="post.pid"
+          cols="12"
+          lg="4"
+          md="6"
+        >   
+          <v-card>
+            <v-list-item>
+              <v-list-item-avatar color="grey"></v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title class="headline">{{ post.title }}</v-list-item-title>
+                <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
 
-    <!-- <v-card
-      max-width="344"
-      class="mx-auto"
-      v-for="post in postList"
-      :key="post.pid"
-    >
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
+              height="194"
+            ></v-img>
 
-      <v-list-item>
-        <v-list-item-avatar color="grey"></v-list-item-avatar>
-        <v-list-item-content>
-          <v-list-item-title class="headline">{{ post.title }}</v-list-item-title>
-          <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
+            <v-card-text>
+              {{ post.content }}
+            </v-card-text>
 
-      <v-img
-        src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
-        height="194"
-      ></v-img>
+            <v-card-actions>
+              <v-btn
+                text
+                color="deep-purple accent-4"
+              >
+                Read
+              </v-btn>
+              <v-btn
+                text
+                color="deep-purple accent-4"
+              >
+                Bookmark
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
 
-      <v-card-text>
-        {{ post.content }}
-      </v-card-text>
-
-      <v-card-actions>
-        <v-btn
-          text
-          color="deep-purple accent-4"
-        >
-          Read
-        </v-btn>
-        <v-btn
-          text
-          color="deep-purple accent-4"
-        >
-          Bookmark
-        </v-btn>
-        <v-spacer></v-spacer>
-        <v-btn icon>
-          <v-icon>mdi-heart</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>mdi-share-variant</v-icon>
-        </v-btn>
-      </v-card-actions>
-    </v-card> -->
 
   </div>
 </template>
@@ -78,11 +79,17 @@ export default {
   created () {
     this.$store.dispatch('getPostList')
     this.postList
+    // console.log(this.postList)
   },
 
 }
 </script>
 
 <style>
-
+  .under-line {
+    height: 0.3rem;
+    width: 3.5rem;
+    margin-bottom: 3rem;
+    background-color: #7C4DFF;
+  }
 </style>
