0246c32e9733f79e603ab6ebafdbb6f362ac008e
YNNJN <dbswls1014@naver.com>
2020-08-18 20:43:44 +0900

    Feat : 메인페이지 카드 컴포넌트 디자인 구현

--- frontend/src/views/contents/mainPostList-2.vue
+++ frontend/src/views/contents/mainPostList-2.vue
@@ -6,6 +6,7 @@
       <div class="under-line"></div>
     </div>
 
+    <!-- TODO: 무한스크롤 적용하기 -->
     <div class="w-75" style="margin-left: 3.5rem;">
       <v-row>
         <v-col
@@ -16,43 +17,40 @@
           md="6"
         >   
           <v-card>
-            <v-list-item>
-              <v-list-item-avatar color="grey"></v-list-item-avatar>
-              <v-list-item-content>
-                <v-list-item-title class="headline">{{ post.title }}</v-list-item-title>
-                <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-
+            <!-- TODO: 포스트 썸네일 가져오기 -->
             <v-img
               src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
               height="194"
             ></v-img>
 
+            <v-list-item>
+              <!-- TODO: 글 작성자의 프로필 사진으로 수정 필요 -->
+              <v-flex style="position: absolute; top: -23px;">
+                <v-icon
+                  v-if="userInfo.picture==null"
+                  style="border: solid 2px #fff"
+                >
+                  mdi-account-circle-outline
+                </v-icon>
+                <v-avatar v-else style="border: solid 2px #fff">
+                  <img :src="userInfo.picture" />
+                </v-avatar>
+              </v-flex>
+
+              <v-list-item-content class="pa-5">
+                <v-list-item-title class="bold" style="font-size: 1.5rem; margin-left: 2.3rem;">{{ post.title }}</v-list-item-title>
+                <v-list-item-subtitle class="regular" style="font-size: 0.9rem; margin-left: 2.3rem;">{{ post.nickname }}</v-list-item-subtitle>                
+              </v-list-item-content>
+            </v-list-item>
+
             <v-card-text>
-              {{ post.content }}
+              <!-- TODO: 컨텐츠 내용 글자 수 제한해서 보여주기 -->
+              {{ post.description }}
             </v-card-text>
 
             <v-card-actions>
-              <v-btn
-                text
-                color="deep-purple accent-4"
-              >
-                Read
-              </v-btn>
-              <v-btn
-                text
-                color="deep-purple accent-4"
-              >
-                Bookmark
-              </v-btn>
-              <v-spacer></v-spacer>
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
+              <!-- TODO: 좋아요 수 가져오기 -->
+              <v-icon small color="#7C4DFF" class="ml-auto">mdi-heart</v-icon>
             </v-card-actions>
           </v-card>
         </v-col>
@@ -74,11 +72,12 @@ export default {
     }
   },
   computed: {
-    ...mapState(["postList"])
+    ...mapState(["postList", "userInfo"])
   },
   created () {
     this.$store.dispatch('getPostList')
     this.postList
+    this.userInfo
     // console.log(this.postList)
   },
 
