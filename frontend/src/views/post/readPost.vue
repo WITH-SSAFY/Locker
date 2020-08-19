0dd647e5d0a084940f649c5ab4f802ef414cadb3
YNNJN <dbswls1014@naver.com>
2020-08-19 16:20:28 +0900

    Feat : 글 상세보기에서 새로운 마크다운 변환 적용

--- frontend/src/views/post/readPost.vue
+++ frontend/src/views/post/readPost.vue
@@ -11,6 +11,7 @@
             <span class="paginate__info">Prev</span>
           </div>
         </v-col>
+
         <v-col cols="10" class="p-0">
           <div class="mx-5 py-3">
             <div class="row post_title ml-1">{{ title }}</div>
@@ -54,9 +55,15 @@
               >{{ tag.tagname }}</v-chip>
             </div>
           </div>
-          <hr />
+
+          <hr>
+
           <div class="row mx-5 py-3">
-            <viewer v-if="flag" :initialValue="viewerText" height="100%" />
+            <!-- 마크다운 뷰어 -->
+            <aside id="markdown" contenteditable style="display: none;">{{ viewerText }}</aside>
+            <section id="output-html" class="markdown-body" style="display: none;"></section>
+            <div id="page" class="markdown-body width: 75%;"></div>
+
           </div>
           <!-- <div>
             <v-btn @click="goPrevPage">이전</v-btn>
@@ -486,7 +493,74 @@ export default {
         })
         .catch(() => alert("첫 페이지 입니다."));
     }
-  }
+  },
+  updated () {
+    function parseMd(md){
+
+      //ul
+      // md = md.replace(/^\s*\n\-\s/gm, '<ul>\n');
+      // md = md.replace(/^(\-.+)\s*([^\-])/gm, '$1\n</ul>\n\n$2');
+      md = md.replace(/^(\-\s)(.+)/gm, '<li>$2</li>');
+      
+      //ol
+      md = md.replace(/^\s*\n\d\./gm, '<ol>\n1.');
+      md = md.replace(/^(\d\..+)\s*\n([^\d\.])/gm, '$1\n</ol>\n\n$2');
+      md = md.replace(/^\d\.(.+)/gm, '<li>$1</li>');
+      
+      //blockquote
+      md = md.replace(/^\>(.+)/gm, '<blockquote>$1</blockquote>');
+      
+      //h
+      md = md.replace(/[\#]{6}\s(.+)/g, '<h6>$1</h6>');
+      md = md.replace(/[\#]{5}\s(.+)/g, '<h5>$1</h5>');
+      md = md.replace(/[\#]{4}\s(.+)/g, '<h4>$1</h4>');
+      md = md.replace(/[\#]{3}\s(.+)/g, '<h3>$1</h3>');
+      md = md.replace(/[\#]{2}\s(.+)/g, '<h2>$1</h2>');
+      md = md.replace(/[\#]{1}\s(.+)/g, '<h1>$1</h1>');
+      
+      //images
+      md = md.replace(/\!\[([^\]]+)\]\(([^\)]+)\)/g, '<img src="$2" alt="$1" />');
+      
+      //links
+      md = md.replace(/[\[]{1}([^\]]+)[\]]{1}[\(]{1}([^\)\"]+)(\"(.+)\")?[\)]{1}/g, '<a href="$2" title="$4">$1</a>');
+      
+      //font styles
+      md = md.replace(/[\*\_]{2}([^\*\_]+)[\*\_]{2}/g, '<b>$1</b>');
+      md = md.replace(/[\*\_]{1}([^\*\_]+)[\*\_]{1}/g, '<i>$1</i>');
+      md = md.replace(/[\~]{2}([^\~]+)[\~]{2}/g, '<del>$1</del>');
+      
+      //pre
+      md = md.replace(/^\s*\n\`\`\`\s(([^\s]+))?/gm, '<pre class="$2">');
+      md = md.replace(/^\`\`\`\s*\n/gm, '</pre>\n\n');
+      
+      //code
+      md = md.replace(/[\`]{1}([^\`]+)[\`]{1}/g, '<code>$1</code>');
+      
+      //hr
+      md = md.replace(/\-\-\-+/gm, '<hr>')
+
+      //p
+      md = md.replace(/^\s*(\n)?(.+)/gm, function(m){
+        return  /\<(\/)?(h\d|ul|ol|li|blockquote|pre|img)/.test(m) ? m : '<p>'+m+'</p>';
+      });
+      
+      //strip p from pre
+      md = md.replace(/(\<pre.+\>)*\<p\>(.+)\<\/p\>/gm, '$1$2');
+      
+      return md;
+
+    }
+
+    var mdEl = document.querySelector('#markdown')
+    var outputEl = document.querySelector('#output-html')
+    var parse = function () {
+      outputEl["innerText"] = parseMd(mdEl.innerText);
+      };
+
+    parse();
+    document.querySelector('#page').innerHTML = document.querySelector('#output-html').innerText
+
+  },  
 };
 </script>
 
