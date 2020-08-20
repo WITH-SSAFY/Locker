d8fa2bfe51bbf2bb3838e0fd4307db74f40fba69
junhok82 <junhok82@gmail.com>
2020-08-21 00:42:28 +0900

    Fix : 글 추적 수정

--- frontend/src/views/post/readPost.vue
+++ frontend/src/views/post/readPost.vue
@@ -619,12 +619,12 @@ export default {
       md = md.replace(/^\>(.+)/gm, "<blockquote>$1</blockquote>");
 
       //h
-      md = md.replace(/[\#]{6}\s(.+)/g, "<h6>$1</h6>");
-      md = md.replace(/[\#]{5}\s(.+)/g, "<h5>$1</h5>");
-      md = md.replace(/[\#]{4}\s(.+)/g, "<h4>$1</h4>");
-      md = md.replace(/[\#]{3}\s(.+)/g, "<h3>$1</h3>");
-      md = md.replace(/[\#]{2}\s(.+)/g, "<h2>$1</h2>");
-      md = md.replace(/[\#]{1}\s(.+)/g, "<h1>$1</h1>");
+      md = md.replace(/[\#]{6}\s(.+)/g, "<h6 id=\"$1\">$1</h6>");
+      md = md.replace(/[\#]{5}\s(.+)/g, "<h5 id=\"$1\">$1</h5>");
+      md = md.replace(/[\#]{4}\s(.+)/g, "<h4 id=\"$1\">$1</h4>");
+      md = md.replace(/[\#]{3}\s(.+)/g, "<h3 id=\"$1\">$1</h3>");
+      md = md.replace(/[\#]{2}\s(.+)/g, "<h2 id=\"$1\">$1</h2>");
+      md = md.replace(/[\#]{1}\s(.+)/g, "<h1 id=\"$1\">$1</h1>");
 
       //images
       md = md.replace(
@@ -676,6 +676,135 @@ export default {
     document.querySelector("#page").innerHTML = document.querySelector(
       "#output-html"
     ).innerText;
+
+   var toc = document.querySelector( '.toc' );
+    var tocPath = document.querySelector( '.toc-marker path' );
+    var tocItems;
+
+    // Factor of screen size that the element must cross
+    // before it's considered visible
+    var TOP_MARGIN = 0.1,
+        BOTTOM_MARGIN = 0.2;
+
+    var pathLength;
+
+    var lastPathStart,
+        lastPathEnd;
+
+    window.addEventListener( 'resize', drawPath, false );
+    window.addEventListener( 'scroll', sync, false );
+
+    drawPath();
+
+    function drawPath() {
+      
+      tocItems = [].slice.call( toc.querySelectorAll( 'li' ) );
+
+      // Cache element references and measurements
+      tocItems = tocItems.map( function( item ) {
+        console.log("item : ",item);
+
+        var anchor = item.querySelector( 'a' );
+        var temp = anchor.getAttribute( 'href' ).slice( 1 );
+        var target = document.getElementById(temp);
+
+        return {
+          listItem: item,
+          anchor: anchor,
+          target: target
+        };
+      } );
+
+      // Remove missing targets
+      tocItems = tocItems.filter( function( item ) {
+        return !!item.target;
+      } );
+
+      var path = [];
+      var pathIndent;
+
+      tocItems.forEach( function( item, i ) {
+
+        var x = item.anchor.offsetLeft - 5,
+            y = item.anchor.offsetTop,
+            height = item.anchor.offsetHeight;
+
+        if( i === 0 ) {
+          path.push( 'M', x, y, 'L', x, y + height );
+          item.pathStart = 0;
+        }
+        else {
+          // Draw an additional line when there's a change in
+          // indent levels
+          if( pathIndent !== x ) path.push( 'L', pathIndent, y );
+
+          path.push( 'L', x, y );
+          
+          // Set the current path so that we can measure it
+          tocPath.setAttribute( 'd', path.join( ' ' ) );
+          item.pathStart = tocPath.getTotalLength() || 0;
+          
+          path.push( 'L', x, y + height );
+        }
+        
+        pathIndent = x;
+        
+        tocPath.setAttribute( 'd', path.join( ' ' ) );
+        item.pathEnd = tocPath.getTotalLength();
+
+      } );
+      
+      pathLength = tocPath.getTotalLength();
+      
+      sync();
+      
+    }
+
+    function sync() {
+      
+      var windowHeight = window.innerHeight;
+      
+      var pathStart = pathLength,
+          pathEnd = 0;
+      
+      var visibleItems = 0;
+      
+      tocItems.forEach( function( item ) {
+
+        var targetBounds = item.target.getBoundingClientRect();
+        
+        if( targetBounds.bottom > windowHeight * TOP_MARGIN && targetBounds.top < windowHeight * ( 1 - BOTTOM_MARGIN ) ) {
+          pathStart = Math.min( item.pathStart, pathStart );
+          pathEnd = Math.max( item.pathEnd, pathEnd );
+          
+          visibleItems += 1;
+          
+          item.listItem.classList.add( 'visible' );
+        }
+        else {
+          item.listItem.classList.remove( 'visible' );
+        }
+        
+      } );
+      
+      // Specify the visible path or hide the path altogether
+      // if there are no visible items
+      if( visibleItems > 0 && pathStart < pathEnd ) {
+        if( pathStart !== lastPathStart || pathEnd !== lastPathEnd ) {
+          tocPath.setAttribute( 'stroke-dashoffset', '1' );
+          tocPath.setAttribute( 'stroke-dasharray', '1, '+ pathStart +', '+ ( pathEnd - pathStart ) +', ' + pathLength );
+          tocPath.setAttribute( 'opacity', 1 );
+        }
+      }
+      else {
+        tocPath.setAttribute( 'opacity', 0 );
+      }
+      
+      lastPathStart = pathStart;
+      lastPathEnd = pathEnd;
+
+    }
+
   },
 };
 </script>
