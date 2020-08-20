81ec49905b116069d57570d90f30d7002ed702f4
YNNJN <dbswls1014@naver.com>
2020-08-20 15:41:30 +0900

    Feat : 팀 레포지토리 화면 수정 완료

--- frontend/src/views/mypage-tabs/manage-repository/AddTeamRepo.vue
+++ frontend/src/views/mypage-tabs/manage-repository/AddTeamRepo.vue
@@ -3,52 +3,58 @@
     <!-- 저장 버튼 -->
     <v-row>
       <v-col class="py-0">
-        <v-btn @click="saveTeam(arrTeamRepo)" style="font-size: 1.5rem; float: right;" text color="#7C4DFF">
-          내용 저장하기
-          <v-icon x-large>save</v-icon>
+        <v-btn @click="saveTeam(arrTeamRepo)" style="font-size: 1.5rem; float: right;" text color="#EDE7F6">
+          <v-icon class="mr-2">save</v-icon>
+          <span class="regular">내용 저장하기</span>
         </v-btn>
       </v-col>
     </v-row>
     <!-- drag and drop : github에서 가져온 리스트 -->
     <v-row>
       <v-col>
-        <div class="p-2 alert-secondary" style="border-radius: 3px;">
-          <div
-            style="font-size: 1.5rem; font-weight: bold;"
-            class="pl-5 pt-2"
-          >My Github Organization</div>
-          <hr />
+        <div class="p-2" style="background-color: #12161A; border-radius: 3px; border: 2px solid #000;">
+
+          <div style="font-size: 1.5rem; padding: 3%;">
+            <span class="light text-white">Github의 팀 레포지토리</span>
+          </div>
+
           <draggable
-            class="list-group kanban-colum"
+            class="list-group kanban-colum p-0 m-0"
             :list="teamGitRepos"
             group="tasks"
-            style="text-align: center;"
+            style="text-align: center; background-color: #12161A;"
           >
             <a
-              class="list-group-item mb-2"
+              class="list-group-item p-0 m-0"
               v-for="element in teamGitRepos"
               :key="element.repoUrl"
-              style="border-radius: 3px;"
+              style="border-radius: 3px; background-color: #12161A;"
             >
               <img alt="left" id="stat" :src="element.src" @click="link(element.repoUrl)">
             </a>
           </draggable>
         </div>
       </v-col>
+
       <!-- drag and drop : locker에 저장된 레포지토리 -->
       <v-col>
-        <div class="p-2" style="background-color: #EDE7F6; border-radius: 3px;">
-          <div
-            style="font-size: 1.5rem; font-weight: bold;"
-            class="pl-5 pt-2"
-          >Locker Team Repository</div>
-          <hr />
-          <draggable class="list-group kanban-colum" :list="arrTeamRepo" group="tasks" style="text-align: center;">
+
+        <div class="p-2" style="background-color: #12161A; border-radius: 3px; border: 1px solid #7C4DFF;">
+
+          <div style="font-size: 1.5rem; padding: 3%;">
+            <span class="light text-white">LOCKER에 저장된 팀 레포지토리</span>
+          </div>
+          
+          <draggable
+            class="list-group kanban-colum"
+            :list="arrTeamRepo" group="tasks"
+            style="text-align: center; background-color: #12161A;"
+          >
             <a
-              class="list-group-item mb-2"
+              class="list-group-item p-0 m-0"
               v-for="element in arrTeamRepo"
               :key="element.repoUrl"
-              style="border-radius: 3px;"
+              style="border-radius: 3px; background-color: #12161A;"
             >
               <img alt="left" id="stat" :src="element.src">
             </a>
